04bd4b4da05e1ffe9be026b287eaae109e8f06d6
Minhducprovip123 <144077732+Minhducprovip123@users.noreply.github.com>
2023-09-26 09:57:57 +0700

    Add files via upload

---
+++ TypeDate/src/MainApp.java
@@ -0,0 +1,37 @@
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName= "Cao Minh";
+		String lastName= "Duc";
+		String id= "100602";
+		String id2= "044202005179";
+		String className="23cd";
+		String schoolName="Cao dang cong nghiep Hue";
+		float mark = 9f;
+		
+		
+		System.out.println("Ho va ten:");
+		System.out.println(firstName + "" + lastName);
+		System.out.println("Ma sinh vien:");
+		System.out.println(id);
+		System.out.println("CCCD:");
+		System.out.println(id2);
+		System.out.println("Lop:");
+		System.out.println(className);
+		System.out.println("Truong:");
+		System.out.println(schoolName);
+		System.out.println("Diem trung binh:");
+		System.out.println(mark);
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
